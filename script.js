5d86c806d026560ba0fd816f7c881123bb9428b8
Nikolay <nyk190@mail.ru>
2022-06-07 21:05:18 +0300

    Add basic structure and styles

---
+++ script.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book(title, author, genre, pages) {
+    this.title = title;
+    this.author = author;
+    this.genre =  genre;
+    this.pages = pages;
+    this.read = false;
+}
+
+function addBookToLibrary() {
+    
+}
