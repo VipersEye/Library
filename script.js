3ea580d9a09694ebe8dec92e23a73d3e4e7f333b
Nikolay <nyk190@mail.ru>
2022-06-17 23:59:40 +0300

    Add toggleBtnNavStatus function
    Add toggleBtnNavStatus function which toggle active class to buttons
    in header section.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let blockNav = document.querySelectorAll('.nav__block');
+let btnsHeader = document.querySelectorAll('.header__btn');
 
 blockNav.forEach(block => {
     block.addEventListener('click', toggleBtnNavStatus);
+});
+
+btnsHeader.forEach(btn => {
+    btn.addEventListener('click', toggleBtnHeaderStatus);
 });
 
 function toggleBtnNavStatus(e) {
@@ -10,4 +15,10 @@ function toggleBtnNavStatus(e) {
         btnNavActive.classList.remove('active');
         e.target.classList.add('active');
     }
+}
+
+function toggleBtnHeaderStatus(e) {
+    let btnHeaderActive = document.querySelector('.header__btn.active');
+    btnHeaderActive.classList.remove('active');
+    e.target.classList.add('active');
 }
