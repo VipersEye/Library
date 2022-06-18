beb122ecbd1d12e72568dc9199319e169f3ede47
Nikolay <nyk190@mail.ru>
2022-06-18 23:14:24 +0300

    Add addition of comments function in aside block
    Add template element for new comments.
    
    Add event listener on document. If you press Enter and input for new
    comment not empty and in focus, event listener calls addComment
    function.
    
    addComment function take template with empty comments, insert text and
    src attribute for user avatar and insert new comment to last comment
    block.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let blockNav = document.querySelectorAll('.nav__block');
 let btnsHeader = document.querySelectorAll('.header__btn');
+let inputAside = document.querySelector('.input_aside');
 
 blockNav.forEach(block => {
     block.addEventListener('click', toggleBtnNavStatus);
@@ -7,6 +8,15 @@ blockNav.forEach(block => {
 
 btnsHeader.forEach(btn => {
     btn.addEventListener('click', toggleBtnHeaderStatus);
+});
+
+document.addEventListener('keypress', (e)=>{
+    if (e.key === 'Enter' && inputAside === document.activeElement && inputAside.value) {
+        let authorAvatar = document.querySelector('.aside__commenting .aside__avatar');
+        let avatarSrc = authorAvatar.getAttribute('src');
+        addComment(inputAside.value, avatarSrc);
+        inputAside.value = '';
+    }
 });
 
 function toggleBtnNavStatus(e) {
@@ -21,4 +31,19 @@ function toggleBtnHeaderStatus(e) {
     let btnHeaderActive = document.querySelector('.header__btn.active');
     btnHeaderActive.classList.remove('active');
     e.target.classList.add('active');
+}
+
+function addComment(text, imgSrc) {
+    let template = document.querySelector('#template_comment');
+    let newComment = template.content.cloneNode('true').querySelector('.aside__comment');
+    
+    let commentText = newComment.querySelector('.aside__comment-text');
+    commentText.textContent = text;
+
+    let commentAvatar = newComment.querySelector('.aside__avatar');
+    commentAvatar.setAttribute('src', imgSrc);
+
+    let lastCommentBlock = document.querySelector('.aside__comment-block:last-child');
+
+    lastCommentBlock.appendChild(newComment);
 }
