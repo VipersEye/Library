9a0fb6c12d1ba3cbf138c7f2c9bce6d0fb27acb7
Nikolay <nyk190@mail.ru>
2022-07-02 00:39:11 +0300

    Add addBookToCollection, removeBookFromCollection
    Add 'in collection' property to all book objects, what indicates
    whether the book is in the main library.
    
    Also add addBookToCollection and removeBookFromCollection function,
    which removes and add books from main library to current user library.

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let mainLibrary = [
         summary: 'Perhaps, 1984 is one of the most famous dystopian novels. The novel was first published in 1906. The main character, Winston Smith, lives in London and works in the Ministry of Truth. Smith is a citizen of the government where all people are under total surveillance and any free-thinking is punished. Winston does not share these ideas, but he carefully hides it, because anyone who is uncovered undergoes an unavoidable "healing" equivalent to mental death.',
         rating: 5,
         'user rating': null,
+        'in collection': false,
         recommends: [],
     },
 
@@ -23,6 +24,7 @@ let mainLibrary = [
         summary: 'Of the five contenders for power, one is dead, another in disfavor, and still the wars rage as violently as ever, as alliances are made and broken. Joffrey, of House Lannister, sits on the Iron Throne, the uneasy ruler of the land of the Seven Kingdoms. His most bitter rival, Lord Stannis, stands defeated and disgraced, the victim of the jealous sorceress who holds him in her evil thrall. But young Robb, of House Stark, still rules the North from the fortress of Riverrun. Robb plots against his despised Lannister enemies, even as they hold his sister hostage at King’s Landing, the seat of the Iron Throne. Meanwhile, making her way across a blood-drenched continent is the exiled queen, Daenerys, mistress of the only three dragons still left in the world...',
         rating: 5,
         'user rating': 4,
+        'in collection': false,
         recommends: [],
     },
 
@@ -35,6 +37,7 @@ let mainLibrary = [
         summary: 'This is the testament of Paul Bäumer, who enlists with his classmates in the German army during World War I. They become soldiers with youthful enthusiasm. But the world of duty, culture, and progress they had been taught breaks in pieces under the first bombardment in the trenches.',
         rating: 4,
         'user rating': null,
+        'in collection': false,
         recommends: [],
     },
 
@@ -47,6 +50,7 @@ let mainLibrary = [
         summary: 'Goethe’s two-part dramatic work, Faust, based on a traditional theme, and finally completed in 1831, is an exploration of that restless intellectual and emotional urge which found its fullest expression in the European Romantic movement, to which Goethe was an early and major contributor. Part I of the work outlines a pact Faust makes with the devil, Mephistopheles, and encompasses the tragedy of Gretchen, whom Faust seduces. Part II, developed over a long period of Goethe’s later life, reflects Goethe’s own transition from a predominantly Romantic to a wider world-view and explores more extensive themes, including the values of the Classical past, as it moves towards the work’s resolution.',
         rating: 3,
         'user rating': 4,
+        'in collection': false,
         recommends: [],
     },
 
@@ -59,6 +63,7 @@ let mainLibrary = [
         summary: 'Presented by Hemingway\'s grandson Seán Hemingway, with a personal foreword by the author’s son Patrick Hemingway, this new enhanced Library Edition of Ernest Hemingway\'s masterpiece about an American in the Spanish Civil War features early drafts and supplementary material, including three previously uncollected short stories on war by one of the greatest writers on the subject in history.',
         rating: 5,
         'user rating': null,
+        'in collection': false,
         recommends: [],
     },
 
@@ -71,6 +76,7 @@ let mainLibrary = [
         summary: 'Narrated by the crew member Ishmael, this epic whaling adventure follows the crew of the Pequod, as its captain, Ahab, descends deeper and deeper into madness on his quest to find and kill the white whale that maimed him. Beyond the surface—of ship life, whaling, and the hunt for the elusive Moby Dick—are allegorical references to life, and even the universe, in this masterpiece by Herman Melville.',
         rating: 5,
         'user rating': 2,
+        'in collection': false,
         recommends: [],
     },
 
@@ -83,6 +89,7 @@ let mainLibrary = [
         summary: 'Sword of Destiny (Polish: Miecz przeznaczenia) is the second book in Andrzej Sapkowski\'s The Witcher series in terms of story chronology, although the original Polish edition was published in 1992, before The Last Wish (but after the Wiedźmin short story collection made obsolete by The Last Wish). Some of the individual short stories were first published in the Fantastyka magazine.',
         rating: 4,
         'user rating': null,
+        'in collection': false,
         recommends: [],
     },
 
@@ -95,6 +102,7 @@ let mainLibrary = [
         summary: 'Thrown in prison for a crime he has not committed, Edmond Dantes is confined to the grim fortress of If. There he learns of a great hoard of treasure hidden on the Isle of Monte Cristo and he becomes determined not only to escape, but also to unearth the treasure and use it to plot the destruction of the three men responsible for his incarceration. Dumas’ epic tale of suffering and retribution, inspired by a real-life case of wrongful imprisonment, was a huge popular success when it was first serialized in the 1840s.',
         rating: 5,
         'user rating': null,
+        'in collection': false,
         recommends: [],
     },
 
@@ -107,6 +115,7 @@ let mainLibrary = [
         summary: 'The second volume in J.R.R. Tolkien\'s epic adventure THE LORD OF THE RINGS One Ring to rule them all, One Ring to find them, One Ring to bring them all and in the darkness bind them Frodo and his Companions of the Ring have been beset by danger during their quest to prevent the Ruling Ring from falling into the hands of the Dark Lord by destroying it in the Cracks of Doom. They have lost the wizard, Gandalf, in a battle in the Mines of Moria. And Boromir, seduced by the power of the Ring, tried to seize it by force. While Frodo and Sam made their escape, the rest of the company was attacked by Orcs. Now they continue the journey alone down the great River Anduin—alone, that is, save for the mysterious creeping figure that follows wherever they go. “Among the greatest works of imaginative fiction of the twentieth century. The book presents us with the richest profusion of new lands and creatures, from the beauty of Lothlórien to the horror of Mordor.”',
         rating: 4,
         'user rating': null,
+        'in collection': false,
         recommends: [],
     },
 ];
@@ -152,7 +161,7 @@ function toggleBtnNavStatus(e) {
     let btnNavActive = this.querySelector('.active');
     if (e.target.closest('.nav__btn')) {
         btnNavActive.classList.remove('active');
-        e.target.classList.add('active');
+        e.target.closest('.nav__btn').classList.add('active');
 
         switch(true) {
             case (Boolean(e.target.closest('#btn-bookstore'))):
@@ -220,17 +229,25 @@ function displayBookCards() {
         bookAuthor.textContent = book.author;
         bookCover.setAttribute('src', `./images/covers/${book.title} _ ${book.author}.jpg`);
 
+        if (book['in collection']) {
+            let btnAdd = bookCard.querySelector('.books__button_add');
+            toggleBtnAddStyles(btnAdd);
+        }
+
         bookCard.addEventListener('click', (e)=>{
             if (e.target.closest('.books__button_info')) {
                 displayBookInfo(e.currentTarget);
             } else if (e.target.closest('.books__star')) {
                 addUserRating(e);
             } else if (e.target.closest('.books__button_add')) {
-                toggleBtnAddStyles(e);
+                toggleBtnAddStyles(e.target);
+                addBookToCollection(e);
             } else if (e.target.closest('.books__button_checked')) {
-                toggleBtnAddStyles(e);
+                toggleBtnAddStyles(e.target);
+                removeBookFromCollection(e);
             }
         });
+        
         let bookImageContainer = bookCard.querySelector('.books__img-container');
         bookImageContainer.addEventListener('mousemove', calculateTransitionOrigin);
 
@@ -362,8 +379,8 @@ function searchBooks(e) {
     displayBookCards();
 }
 
-function toggleBtnAddStyles(e) {
-    let btnAdd = e.target.closest('.books__button');
+function toggleBtnAddStyles(target) {
+    let btnAdd = target.closest('.books__button');
     let btnAddSvg = btnAdd.querySelector('.books__icon');
     let btnAddIcon = btnAddSvg.querySelector('use');
     if (btnAdd.classList.contains('books__button_add')) {
@@ -375,4 +392,18 @@ function toggleBtnAddStyles(e) {
     btnAdd.classList.toggle('books__button_checked');
     btnAddSvg.classList.toggle('books__icon_add');
     btnAddSvg.classList.toggle('books__icon_checked');
+}
+
+function addBookToCollection(e) {
+    let bookId = e.target.closest('.books__card').getAttribute('book_id');
+    let newBook = mainLibrary.find( book => book.id === +bookId );
+    newBook['in collection'] = true;
+    currentUser.library.push(newBook);
+}
+
+function removeBookFromCollection(e) {
+    let bookId = e.target.closest('.books__card').getAttribute('book_id');
+    let book = mainLibrary.find( book => book.id === +bookId );
+    book['in collection'] = false;
+    currentUser.library = currentUser.library.filter( book => book.id !== +bookId );
 }
